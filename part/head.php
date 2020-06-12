b952f9b9f6a62300fa9c37a3c590f59dd9db49d5
Milly55 <mn02006@daum.net>
2020-06-12 18:37:33 +0900

    블로그 메뉴

---
+++ part/head.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>블로그</title> 
+    <link rel="stylesheet" href="/resource/common.css">
+    <link rel="stylesheet" href="/resource/common.js">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+</head>
+<body>
+    <div class="top-bar">
+    <nav class="menu-box-1 con flex">
+    <ul class="flex flex-1-0-0 ">
+        <li class="flex flex-1-0-0 "><a href="/" class="flex-1-0-0 ">Home</a></li>
+        <li class="flex flex-1-0-0 "><a href="/list.php" class="flex-1-0-0 ">Articles</a></li>
+        <li class="flex flex-1-0-0 "><a href="/about me.php" class="flex-1-0-0 ">abut me</a></li>
+        <li class="flex flex-1-0-0 "><a href="#" class="flex-1-0-0 ">sns</a>
+    <ul class="flex ">
+        <li class="flex "><a href="#"><i class="fab fa-instagram"></i>인스타그램</a></li>
+        <li class="flex "><a href="#"><i class="fab fa-facebook-square"></i>페이스북</a></li>
+    </ul> 
+</li>
+    </ul> 
+    </nav>
+    </div>
