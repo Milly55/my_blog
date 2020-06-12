82db9845be29f48268510fd56c7b62f1e6ba1afc
Milly55 <mn02006@daum.net>
2020-06-12 22:41:57 +0900

    탑바작업

--- part/head.php
+++ part/head.php
@@ -6,11 +6,19 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>블로그</title> 
     <link rel="stylesheet" href="/resource/common.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="/resource/common.js">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
 </head>
 <body>
     <div class="top-bar">
+    <div class="mobile-top-bar visible-sm-down flex">
+        <a href="#" class="btn-toggle-mobile-side-bar flex">
+            <div></div>
+            <div></div>
+            <div></div>
+        </a>
+    </div>
     <nav class="menu-box-1 con flex">
     <ul class="flex flex-1-0-0 ">
         <li class="flex flex-1-0-0 "><a href="/" class="flex-1-0-0 ">Home</a></li>
