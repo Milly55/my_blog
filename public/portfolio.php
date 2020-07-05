fc197fc6e6529933a4fdd0fa5d0a80ee4c69c28b
Milly55 <mn02006@daum.net>
2020-07-05 17:59:50 +0900

    블로그 포트폴리오수정

--- public/portfolio.php
+++ public/portfolio.php
@@ -6,13 +6,31 @@ include "../part/head.php";
 
 
 
-<div class="main-bar">
-    <div class="main-box con flex">    
-<div class="img-box-1 flex">
-<ul class=" flex">
-    <li><a href="/portfolio1.html" target="_blank"><div class="img-box" style="background-image:url(http://www.icounseling.co.kr/Resource/upload_img/20190715130200023.jpg);"></div></a></li>
-</ul>
-
+<div class="portfolio-bar ">
+    <div class="portfolio-box  con ">    
+<div class="portfolio-img-box-1 flex">
+    <ul class="flex">
+        <li class="flex"><a href="/portfolio1.html" target="_blank">
+        <div class="img-box" style="background-image:url(http://www.icounseling.co.kr/images/bg_main_section_0.jpg);"></div>
+        <h3>아이심리상담소 </h3>(사이트 모작) <div class="portfolio-txt">기존과 동일하게 제작하였습니다.</div></a></li>
+        <li class="flex"><a href="">
+        <div class="img-box" style="background-image:url(https://ifh.cc/g/Mh3Z9D.png);"></div>
+        <h3>솔라시도 모작</h3>(사이트 모작, 반응형) <div class="portfolio-txt">기존과 동일하게 제작하였습니다.</div></a></li>
+        <li class="flex"><a href="">
+        <div class="img-box" style="background-image:url(http://www.icounseling.co.kr/images/bg_main_section_0.jpg);"></div>
+        <h3>제목</h3>(사이트 모작) <div class="portfolio-txt">내용</div></a></li>
+    </ul>
+    <ul class="flex">
+        <li class="flex"><a href="/portfolio1.html" target="_blank">
+        <div class="img-box" style="background-image:url(http://www.icounseling.co.kr/images/bg_main_section_0.jpg);"></div>
+        <h3>제목 </h3>(특징) <div class="portfolio-txt">내용</div></a></li>
+        <li class="flex"><a href="">
+        <div class="img-box" style="background-image:url(https://ifh.cc/g/Mh3Z9D.png);"></div>
+        <h3>제목</h3>(특징) <div class="portfolio-txt">내용</div></a></li>
+        <li class="flex"><a href="">
+        <div class="img-box" style="background-image:url(http://www.icounseling.co.kr/images/bg_main_section_0.jpg);"></div>
+        <h3>제목</h3>(특징) <div class="portfolio-txt">내용</div></a></li>
+    </ul>
   </div>
 </div>
 </div>
