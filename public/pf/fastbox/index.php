07be446d21a2b3691d20d56f7ce76c232653c5b2
Milly55 <mn02006@daum.net>
2020-07-27 18:37:59 +0900

    패스트박스

---
+++ public/pf/fastbox/index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    <link rel="stylesheet" href="/pf/fastbox/index.css">
+    <script src="/pf/fastbox/index.js"></script>
+    <title>패스트박스</title>
+</head>
+<body>
+<section class="top-mneu-bar">
+    <div class="menu-box con flex">
+        <div class="logo-box" style="margin-left:20px;"><a href="#"><img src="https://milly55.github.io/img1/pf/fastbox/logo.png" alt=""></a></div>
+        <ul class="flex">
+            <li class="flex"><a href="#">회사소개</a>
+                <ul class="flex">
+                    <li><a href="#">FASTBOX</a></li>
+                    <li><a href="#">HISTORY</a></li>
+                    <li><a href="#">VISION & MISSION</a></li>
+                    <li><a href="#">CI</a></li>
+                    <li><a href="#">NETWORK</a></li>
+                </ul>
+            </li>
+            <li class="flex"><a href="#">서비스소개</a>
+                <ul>
+                    <li><a href="#">이커먼스 플필먼트</a></li>
+                    <li><a href="#">크로스 보더</a></li>
+                    <li><a href="#">드랍쉬핑</a></li>
+                    <li><a href="#">특화 서비스</a></li>
+                </ul>
+            </li>
+            <li class="flex"><a href="#">인재경영</a>
+                <ul>
+                    <li><a href="#">인재상</a></li>
+                    <li><a href="#">인재제도</a></li>
+                    <li><a href="#">복리후생</a></li>
+                    <li><a href="#">채용안내</a></li>
+                </ul>
+            </li>
+            <li class="flex"><a href="#">고객센터</a>
+                <ul>
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">Q&A</a></li>
+                    <li><a href="#">CONTACT US</a></li>
+                    <li><a href="#">COST CALCULATOR</a></li>
+                </ul>
+            </li>
+        </ul>
+        <div class="language-box flex">
+            <ul class="flex">
+                <li><a href="#">KOR</a></li>
+                <li><a href="#">ENG</a></li>
+                <li><a href="#">CHN</a></li>
+                <li><a href="#">JPN</a></li>
+                <li style="margin-left:20px;"><a href="#"><i class="fas fa-bars"></i></a></li>
+            </ul>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+<div class="slider-k slider-k1 height-100vh" data-autoplay="Y" data-autoplay-dir="right" data-autoplay-interval="7000">
+    <div class="slides">
+        <div class="active" style="background-image:url(https://milly55.github.io/img1/pf/fastbox/main_visual_01.jpg)"></div>
+        <div style="background-image:url(https://milly55.github.io/img1/pf/fastbox/main_visual_02.jpg)"></div>
+        <div style="background-image:url(https://milly55.github.io/img1/pf/fastbox/main_visual_03.jpg)"></div>
+    </div>
+
+    <div class="side-btns flex">
+        <div class="dot-box flex">
+            <div class="flex">
+                <span>
+                   <img src="https://milly55.github.io/img1/pf/fastbox/main_prev_btn.png" alt="">
+                </span>
+            </div>
+            <div class="flex">
+                <span>
+                    <img src="https://milly55.github.io/img1/pf/fastbox/main_next_btn.png" alt="">
+                </span>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
