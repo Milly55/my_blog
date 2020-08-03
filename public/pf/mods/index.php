972c1d8966502500971ec5a95d0d38a065efe9a4
Milly55 <mn02006@daum.net>
2020-08-03 18:47:46 +0900

    수정

--- public/pf/mods/index.php
+++ public/pf/mods/index.php
@@ -16,7 +16,7 @@
     <title>모즈 클리닉</title>
 </head>
 <body>
-    
+
 <div class="top-bar con">
     <div class="menu-bar ">
         <div class="background"></div>
@@ -218,7 +218,6 @@
                 </ul>
             </div>
         </div>
-
         <div class="middle-box-3 flex ">
             <div class="background-img-2" style="background-image:url(https://milly55.github.io/img1/pf/mods/event_bg.jpg)">
             </div>
@@ -273,8 +272,88 @@
                 </div>
             </div>
         </div>
+        <div class="middle-box-4 flex">
+            <div class="background-img-3" style="background-image:url(https://milly55.github.io/img1/pf/mods/green_bg.jpg)"></div>
+            <div class="middle-title-4 flex">
+                <ul class="brand-txt-1 flex">
+                    <li>MODS BRAND</li>
+                    <li>삶의 가치를 더 행복하고 건강하게 만듭니다.</li>
+                </ul>
+                <ul class="brand-txt-2 flex">
+                    <li>MODS Package</li>
+                    <li>나의 몸을 나보다 더 신경 쓰는 서비스, 우리는 그것을 <br> 안티에이징 버틀러 라고 부릅니다.</li>
+                    <li><a href="#">View Detail </a> </li>
+                </ul>
+            </div>
+        </div>
+        <div class="middle-box-4-a flex">
+            <ul class="flex">
+                <li>We will offer well aging beyond anti-aging.</li>
+                <li>모즈클리닉은 고객의 삶에 있어 행복이 너무나 귀중하다는 것을 잘 알고 있습니다.</li>
+                <li>여기저기 흔하게 나와 있는 진료 방법을 적용하기는 쉽습니다. 그러나 고객 한 분을 깊이 있게 이해하고</li>
+                <li>조화롭게 진료 방법을 적용해 나가는 일은 어렵습니다. 모즈클리닉은 청담동에 위치한 뛰어난 접근성과</li>
+                <li>프라이빗한 진료, 퀄리티, 최상의 서비스, 패키지로 여유롭고 확실한 결과, 그림자 같은 서비스로 쉼터를 제공하고 있습니다.</li>
+            </ul>
+        </div>
+        <div class="middle-box-5">
+            <div class="background-img-4" style="background-image:url(https://milly55.github.io/img1/pf/mods/purple_bg.jpg)"></div>
+            <div class="middle-title-5">
+                <ul class="flex">
+                    <li>MODS GYM Center</li>
+                    <li>모즈클리닉은 지속적인 안티에이징 모니터링과</li>
+                    <li>관리를 통한 웰에이징을 추구합니다.</li>
+                    <li><a href="#">View Detail </a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="middle-box-5-a flex">
+            <ul class="flex">
+                <li>True anti-aging is effective in comprehensive management.</li>
+                <li>개인마다 다른 집중도와 비율을 안배 하여야 함에도 불구하고 한 가지 진료 수단으로 끝내는경우가 많습니다.</li>
+                <li>시간과 비용을 들였음에도 효과적인 결과를 내지 못하는 이유는 목적으로 접근하지 않고 진료 방법에 초점을 맞춘 경우입니다.</li>
+                <li>모즈클리닉에서는 각기 다른 고객의 목적에 맞는 패키지를 통해 가장 효과적인 결과를 선사해 드리고 있습니다.</li>
+            </ul>
+        </div>
+    </div>
+    <div class="slider-k slider-k1 height-100vh" data-autoplay="Y" data-autoplay-dir="right" data-autoplay-interval="7000">
+        <div class="slides">
+            <div class="active" style="background-image:url(https://milly55.github.io/img1/pf/mods/hosp_4.jpg)">
+                <div class="background-1" style="background-image:url(https://milly55.github.io/img1/pf/mods/hosp_1.jpg)"></div>
+            </div>
+            <div style="background-image:url(https://milly55.github.io/img1/pf/mods/hosp_1.jpg)">
+                <div class="background-1" style="background-image:url(https://milly55.github.io/img1/pf/mods/hosp_2.jpg)"></div>
+            </div>
+            <div style="background-image:url(https://milly55.github.io/img1/pf/mods/hosp_2.jpg)">
+                <div class="background-1" style="background-image:url(https://milly55.github.io/img1/pf/mods/hosp_3.jpg)"></div>
+            </div>
+            <div style="background-image:url(https://milly55.github.io/img1/pf/mods/hosp_3.jpg)">
+                <div class="background-1" style="background-image:url(https://milly55.github.io/img1/pf/mods/hosp_4.jpg)"></div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="foot-bar flex">
+        <div class="foot-box con flex">
+            <ul class="foot-txt-1  flex">
+                <li>서울특별시 강남구 압구정로80길 35, 1층 (청담동, 드림빌딩) 팩스: 02.516.1618</li>
+                <li>의료기관명: 바른건강의료소비자생활협동조합 모즈의원 사업자등록 : 287-48-00521 대표 : 손승용</li>
+            </ul>
+            <ul class="foot-txt-2  flex">
+                <li>월요일 ~ 금요일 : <span>10:00 ~ 19:00</span>     |  토요일 ~ 일요일 : <span>10:00 ~ 17:00</span>   </li>
+                <li>*공휴일은 당일 휴진입니다.</li>
+                <li>상담전화 <span> 02.545.1618</span>  | <a href="#">찾아오시는 길</a> </li>
+            </ul>
+            <ul class=foot-txt-3 "flex">
+                <li class="flex"><span><a href="#"><img src="https://milly55.github.io/img1/pf/mods/u_btn_blog.png" alt=""></a> </span>
+                    <span><a href="#"><img src="https://milly55.github.io/img1/pf/mods/u_btn_insta.png" alt=""></a></span> 
+                    <span><a href="#"><img src="https://milly55.github.io/img1/pf/mods/u_btn_you.png" alt=""></a> </span></li>
+                <li class="flex">© MODS CLINIC. All Rights Reserved.</li>
+                <li class="flex"><a href="#">Designed by Chairone</a> </li>
+            </ul>
+        </div>
     </div>
 </div>
+
 
 <!--  
 
